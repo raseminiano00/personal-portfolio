1834251b301cdd5a9ca2af510110fa974bfd29f1
roi <raseminiano00@gmail.com>
2022-04-22 16:36:09 +0800

    Remove routers as it is not neede

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Works from "./components/Works";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Router>
-        <Switch>
-          <Route exact path={["/", "/#about", "/#projects"]}>
-            <Banner />
-            <Works />
-          </Route>
-        </Switch>
-      </Router>
+      <Banner />
+      <Works />
     </>
   );
 }
