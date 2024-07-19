11472c1db769949847373c252acb0ae95b532c0f
roi <raseminiano00@gmail.com>
2024-07-19 18:33:31 +0800

    Update icons

--- src/components/ReachMe.js
+++ src/components/ReachMe.js
@@ -5,26 +5,38 @@ import email from "../assets/email.png";
 
 const ReachMe = () => {
   return (
-    <section id="reach-me" className="h-[180px] md:max-h-[1200px] mt-24">
-      <div className="flex justify-center size-full bg-neutral-100 ">
+    <section
+      id="reach-me"
+      className="h-[180px] md:max-h-[1200px] mt-24 bg-neutral-100 "
+    >
+      <div className="flex justify-center size-full pt-8">
         <div class="w-5/12 my-auto flex items-center flex-col gap-2">
           <p class="mx-auto text-md md:text-lg">Roi Angelo Seminiano</p>
 
           <span class="mx-auto flex flex-row items-center gap-2">
-            <img className="size-4" src={email} alt="icon"></img>
-            <a class="text-xs md:text-sm" href="mailto:raseminiano00@gmail.com">
+            <img className="w-3 h-3" src={email} alt="icon"></img>
+            <a
+              target="_blank"
+              class="text-xs md:text-sm"
+              href="mailto:raseminiano00@gmail.com"
+            >
               raseminiano00@gmail.com
             </a>
           </span>
           <span class="mx-auto flex flex-row items-center gap-2">
-            <img className="size-4" src={phFlag} alt="icon"></img>
-            <a class="text-xs md:text-sm" href="tel:+639208339508">
-              +63 920 833 9508
+            <img className="w-3 h-3" src={phFlag} alt="icon"></img>
+            <a
+              target="_blank"
+              class="text-xs md:text-sm"
+              href="tel:+639624200252"
+            >
+              +63 962 420 0252
             </a>
           </span>
           <span class="mx-auto flex flex-row items-center gap-2">
-            <img className="size-5" src={linkedin} alt="icon"></img>
+            <img className="w-4 h-4" src={linkedin} alt="icon"></img>
             <a
+              target="_blank"
               class="text-xs md:text-sm"
               href="https://www.linkedin.com/in/roi-angelo-seminiano"
             >
